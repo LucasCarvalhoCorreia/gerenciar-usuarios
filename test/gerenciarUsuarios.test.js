a3ef4c5240f0d363230a83a98ccce207f1ceb254
Lucas Correia <lucas.carvalho79@hotmail.com>
2025-09-04 22:50:49 -0300

    Criando funções e testes de criação de novos usuários

---
+++ test/gerenciarUsuarios.test.js
@@ -0,0 +1,22 @@
+const { retornarUsuarios } = require('../src/gerenciarUsuarios');
+const { adicionarNovoUsuario } = require('../src/gerenciarUsuarios');
+
+//Faz as comparações
+const { expect } = require('chai');
+
+describe('Testes de adição de novos nomes de usuário', function () {
+
+    it('Validar se um novo usuário foi adicionado corretamente', function () {
+        adicionarNovoUsuario('Murilo');
+
+        const listaDeUsuarios = retornarUsuarios();
+        
+        expect(listaDeUsuarios.at(-1)).to.equal('Murilo');
+    });
+
+    it('Validar se a lista de usuários é retornada corretamente', function () {
+        const listaDeUsuarios = retornarUsuarios();
+        
+        expect(listaDeUsuarios.length).to.be.greaterThan(0);
+    });
+});
